587b7906adc0845def8787ef099fee2edcacea08
Osama Fahim <osama.fahim@solera.com>
2025-01-06 14:39:53 +0100

    Initial Commit

---
+++ SwiftUI StarterPack/SwiftUI_StarterPackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_StarterPackApp.swift
+//  SwiftUI StarterPack
+//
+//  Created by Osama Fahim on 6/1/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_StarterPackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
